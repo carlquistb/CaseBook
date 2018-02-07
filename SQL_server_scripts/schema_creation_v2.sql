eb3a476b572a90f488bafdcf876abc308f4bc134
Brendan Carlquist <carlquistb@gmail.com>
2018-02-06 16:27:41 -0800

    added some named constraints.

--- SQL_server_scripts/schema_creation_v2.sql
+++ SQL_server_scripts/schema_creation_v2.sql
@@ -6,68 +6,80 @@ create database CASEBOOK_V2
 go
 use CASEBOOK_V2
 go
-create table [FACILITY_TYPE]
+create table [tblFACILITY_TYPE]
 	(
-		FacilityTypeID int identity(1,1) primary key,
+		FacilityTypeID int identity(1,1),
 		FacilityTypeName nvarchar(100) not null,
 		FacilityTypeDescr nvarchar(500)
+
+		constraint pk_tblFACILITY_TYPE primary key (FacilityTypeID)
 	)
-create table [FACILITY]
+create table [tblFACILITY]
 	(
-		FacilityID int identity(1,1) primary key,
+		FacilityID int identity(1,1),
 		FacilityName nvarchar(100) not null,
 		FacilityDescr nvarchar(500),
-		FacilityTypeID int not null foreign key references [FACILITY_TYPE](FacilityTypeID)
+		FacilityTypeID int not null
+
+		constraint pk_tblFACILITY primary key (FacilityID),
+		constraint fk_tblFACILITY_FacilityTypeID foreign key (FacilityTypeID) references [tblFACILITY_TYPE](FacilityTypeID)
 	)
-create table [EQUIPMENT_TYPE]
+create table [tblEQUIPMENT_TYPE]
 	(
-		EquipmentTypeID int identity(1,1) primary key,
+		EquipmentTypeID int identity(1,1),
 		EquipmentTypeName nvarchar(100) not null,
 		EquipmentTypeDescr nvarchar(500)
+
+		constraint pk_tblEQUIPMENT_TYPE primary key (EquipmentTypeID)
 	)
-create table [EQUIPMENT_SUBTYPE]
+create table [tblEQUIPMENT_SUBTYPE]
 	(
-		EquipmentSubtypeID int identity(1,1) primary key,
+		EquipmentSubtypeID int identity(1,1),
 		EquipmentSubtypeName nvarchar(100) not null,
 		EquipmentSubtypeDescr nvarchar(500),
-		EquipmentTypeID int not null foreign key references [EQUIPMENT_TYPE](EquipmentTypeID)
+		EquipmentTypeID int not null foreign key references [tblEQUIPMENT_TYPE](EquipmentTypeID)
+
+		constraint pk_tblEQUIPMENT_SUBTYPE primary key (EquipmentSubtypeID),
+		constraint fk_tblEQUIPMENT_SUBTYPE_EquipmentTypeID foreign key (EquipmentTypeID) references [tblEQUIPMENT_TYPE](EquipmentTypeID)
 	)
-create table [EQUIPMENT]
+create table [tblEQUIPMENT]
 	(
-		EquipmentID int identity(1,1) primary key,
+		EquipmentID int identity(1,1),
 		EquipmentName nvarchar(100) not null,
 		EquipmentDescr nvarchar(500),
 		ReplacementCost money,
-		EquipmentTypeID int not null foreign key references [EQUIPMENT_SUBTYPE](EquipmentSubtypeID)
+		EquipmentTypeID int not null foreign key references [tblEQUIPMENT_SUBTYPE](EquipmentSubtypeID)
+
+		constraint pk_tblEQUIPMENT primary key (EquipmentID)
 	)
-create table [EQUIPMENT_FACILITY]
+create table [tblEQUIPMENT_FACILITY]
 	(
 		EqFacID int identity(1,1) primary key,
-		EqID int not null foreign key references [EQUIPMENT](EquipmentID),
-		FacID int not null foreign key references [FACILITY](FacilityID)
+		EqID int not null foreign key references [tblEQUIPMENT](EquipmentID),
+		FacID int not null foreign key references [tblFACILITY](FacilityID)
 	)
-create table [FEE_TYPE]
+create table [tblFEE_TYPE]
 	(
 		FeeTypeID int identity(1,1) primary key,
 		FeeTypeName nvarchar(100) not null,
 		FeeTypeDescr nvarchar(500)
 	)
-create table [FEE]
+create table [tblFEE]
 	(
 		FeeID int identity(1,1) primary key,
 		FeeName nvarchar(100) not null,
 		FeeDescr nvarchar(500),
 		FeeAmount money,
-		FeeTypeID int not null foreign key references [FEE_TYPE](FeeTypeID)
+		FeeTypeID int not null foreign key references [tblFEE_TYPE](FeeTypeID)
 	)
-create table [FEE_HISTORY]
+create table [tblFEE_HISTORY]
 	(
 		FeeHistoryID int identity primary key,
-		FeeID int not null foreign key references [FEE](FeeID),
+		FeeID int not null foreign key references [tblFEE](FeeID),
 		FeeAmount money,
 		FeeHistoryDate date
 	)
-create table [PERSON]
+create table [tblPERSON]
 	(
 		PersonID int identity(1,1) primary key,
 		Fname nvarchar(100) not null,
@@ -76,54 +88,54 @@ create table [PERSON]
 		BirthDate date not null,
 		Email nvarchar(500)
 	)
-create table [ROLE]
+create table [tblROLE]
 	(
 		RoleID int identity(1,1) primary key,
 		RoleName nvarchar(100) not null,
 		RoleDescr nvarchar(500)
 	)
-create table [PERSON_ROLE]
+create table [tblPERSON_ROLE]
 	(
 		P_R_ID int identity(1,1) primary key,
-		PersonID int not null foreign key references [PERSON](PersonID),
-		RoleID int not null foreign key references [ROLE](RoleID),
+		PersonID int not null foreign key references [tblPERSON](PersonID),
+		RoleID int not null foreign key references [tblROLE](RoleID),
 		BeginDate date,
 		EndDate date
 	)
-create table [EVENT_TYPE]
+create table [tblEVENT_TYPE]
 	(
 		EventTypeID int identity(1,1) primary key,
 		EventTypeName nvarchar(100) not null,
 		EventTypeDescr nvarchar(500)
 	)
-create table [TRANSMITTAL]
+create table [tblTRANSMITTAL]
 	(
 		TransmittalID int identity(1,1) primary key,
 		TransmittalName nvarchar(100) not null,
 		TransmittalAmount money,
 		TransmittalDate date
 	)
-create table [EVENT]
+create table [tblEVENT]
 	(
 		EventID int identity(1,1) primary key,
 		EventName nvarchar(100) not null,
 		EventDate date not null,
-		EventTypeID int not null foreign key references [EVENT_TYPE](EventTypeID),
-		EquipmentID int not null foreign key references [EQUIPMENT](EquipmentID),
-		FeeID int not null foreign key references [FEE](FeeID),
-		P_R_ID int not null foreign key references [PERSON_ROLE](P_R_ID),
+		EventTypeID int not null foreign key references [tblEVENT_TYPE](EventTypeID),
+		EquipmentID int not null foreign key references [tblEQUIPMENT](EquipmentID),
+		FeeID int not null foreign key references [tblFEE](FeeID),
+		P_R_ID int not null foreign key references [tblPERSON_ROLE](P_R_ID),
 	)
-create table [EVENT_TRANSMITTAL]
+create table [tblEVENT_TRANSMITTAL]
 	(
 		EventTransmittalID int identity(1,1) primary key,
-		EventID int not null foreign key references [EVENT](EventID),
-		TRANSMITTALID int not null foreign key references [TRANSMITTAL](TransmittalID),
+		EventID int not null foreign key references [tblEVENT](EventID),
+		TRANSMITTALID int not null foreign key references [tblTRANSMITTAL](TransmittalID),
 		amount money
 	)
-create table [COMMENT]
+create table [tblCOMMENT]
 	(
 		CommentID int identity(1,1) primary key,
 		CommentDate date not null,
-		EventID int not null foreign key references [EVENT](EventID),
+		EventID int not null foreign key references [tblEVENT](EventID),
 		CommentBody nvarchar(1000)
 	)
