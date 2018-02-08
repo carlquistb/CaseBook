ea15a6ac70f7a16d9dfce1c1347667e9fcd56168
Brendan Carlquist <carlquistb@gmail.com>
2018-02-07 22:04:05 -0800

    added the rest of the explicit primary and foreign key restraints

--- SQL_server_scripts/schema_creation_v2.sql
+++ SQL_server_scripts/schema_creation_v2.sql
@@ -37,7 +37,7 @@ create table [tblEQUIPMENT_SUBTYPE]
 		EquipmentSubtypeID int identity(1,1),
 		EquipmentSubtypeName nvarchar(100) not null,
 		EquipmentSubtypeDescr nvarchar(500),
-		EquipmentTypeID int not null foreign key references [tblEQUIPMENT_TYPE](EquipmentTypeID)
+		EquipmentTypeID int not null
 
 		constraint pk_tblEQUIPMENT_SUBTYPE primary key (EquipmentSubtypeID),
 		constraint fk_tblEQUIPMENT_SUBTYPE_EquipmentTypeID foreign key (EquipmentTypeID) references [tblEQUIPMENT_TYPE](EquipmentTypeID)
@@ -48,94 +48,130 @@ create table [tblEQUIPMENT]
 		EquipmentName nvarchar(100) not null,
 		EquipmentDescr nvarchar(500),
 		ReplacementCost money,
-		EquipmentTypeID int not null foreign key references [tblEQUIPMENT_SUBTYPE](EquipmentSubtypeID)
+		EquipmentSubtypeID int not null
 
 		constraint pk_tblEQUIPMENT primary key (EquipmentID)
+		constraint fk_tblEQUIPMENT_EquipmentSubtypeID foreign key (EquipmentSubtypeID) references [tblEQUIPMENT_SUBTYPE](EquipmentSubtypeID)
 	)
 create table [tblEQUIPMENT_FACILITY]
 	(
-		EqFacID int identity(1,1) primary key,
+		EqFacID int identity(1,1),
 		EqID int not null foreign key references [tblEQUIPMENT](EquipmentID),
 		FacID int not null foreign key references [tblFACILITY](FacilityID)
+
+		constraint pk_tblEQUIPMENT_FACILITY primary key (EqFacID)
 	)
 create table [tblFEE_TYPE]
 	(
-		FeeTypeID int identity(1,1) primary key,
+		FeeTypeID int identity(1,1),
 		FeeTypeName nvarchar(100) not null,
 		FeeTypeDescr nvarchar(500)
+
+		constraint pk_tblFEE_TYPE primary key (FeeTypeID)
 	)
 create table [tblFEE]
 	(
-		FeeID int identity(1,1) primary key,
+		FeeID int identity(1,1),
 		FeeName nvarchar(100) not null,
 		FeeDescr nvarchar(500),
 		FeeAmount money,
-		FeeTypeID int not null foreign key references [tblFEE_TYPE](FeeTypeID)
+		FeeTypeID int not null
+
+		constraint pk_tblFEE primary key (FeeID),
+		constraint fk_tblFEE_FeeTypeID foreign key (FeeTypeID) references [tblFEE_TYPE](FeeTypeID)
 	)
 create table [tblFEE_HISTORY]
 	(
-		FeeHistoryID int identity primary key,
-		FeeID int not null foreign key references [tblFEE](FeeID),
+		FeeHistoryID int identity,
+		FeeID int not null,
 		FeeAmount money,
 		FeeHistoryDate date
+
+		constraint pk_tblFEE_HISTORY primary key (FeeHistoryID),
+		constraint fk_tblFEE_HISTORY_FeeID foreign key (FeeID) references [tblFEE](FeeID)
 	)
 create table [tblPERSON]
 	(
-		PersonID int identity(1,1) primary key,
+		PersonID int identity(1,1),
 		Fname nvarchar(100) not null,
 		Lname nvarchar(100) not null,
 		Mname nvarchar(100),
 		BirthDate date not null,
 		Email nvarchar(500)
+
+		constraint pk_tblPERSON primary key (PersonID)
 	)
 create table [tblROLE]
 	(
-		RoleID int identity(1,1) primary key,
+		RoleID int identity(1,1),
 		RoleName nvarchar(100) not null,
 		RoleDescr nvarchar(500)
+
+		constraint pk_tblROLE primary key (RoleID)
 	)
 create table [tblPERSON_ROLE]
 	(
-		P_R_ID int identity(1,1) primary key,
-		PersonID int not null foreign key references [tblPERSON](PersonID),
-		RoleID int not null foreign key references [tblROLE](RoleID),
+		P_R_ID int identity(1,1),
+		PersonID int not null,
+		RoleID int not null),
 		BeginDate date,
 		EndDate date
+
+		constraint pk_tblPERSON_ROLE primary key (P_R_ID),
+		constraint fk_tblPERSON_ROLE_PersonID foreign key (PersonID) references [tblPERSON](PersonID),
+		constraint fk_tblPERSON_ROLE_RoleID foreign key (RoleID) references [tblROLE](RoleID)
 	)
 create table [tblEVENT_TYPE]
 	(
-		EventTypeID int identity(1,1) primary key,
+		EventTypeID int identity(1,1),
 		EventTypeName nvarchar(100) not null,
 		EventTypeDescr nvarchar(500)
+
+		constraint pk_tblEVENT_TYPE primary key (EventTypeID)
 	)
 create table [tblTRANSMITTAL]
 	(
-		TransmittalID int identity(1,1) primary key,
+		TransmittalID int identity(1,1),
 		TransmittalName nvarchar(100) not null,
 		TransmittalAmount money,
 		TransmittalDate date
+
+		constraint pk_tblTRANSMITTAL primary key (TransmittalID)
 	)
 create table [tblEVENT]
 	(
-		EventID int identity(1,1) primary key,
+		EventID int identity(1,1),
 		EventName nvarchar(100) not null,
 		EventDate date not null,
-		EventTypeID int not null foreign key references [tblEVENT_TYPE](EventTypeID),
-		EquipmentID int not null foreign key references [tblEQUIPMENT](EquipmentID),
-		FeeID int not null foreign key references [tblFEE](FeeID),
-		P_R_ID int not null foreign key references [tblPERSON_ROLE](P_R_ID),
+		EventTypeID int not null,
+		EquipmentID int not null,
+		FeeID int not null,
+		P_R_ID int not null
+
+		constraint pk_tblEVENT primary key (EventID),
+		constraint fk_tblEVENT_EventTypeID foreign key (EventTypeID) references [tblEVENT_TYPE](EventTypeID),
+		constraint fk_tblEVENT_EquipmentID foreign key (EquipmentID) references [tblEQUIPMENT](EquipmentID),
+		constraint fk_tblEVENT_FeeID foreign key (FeeID) references [tblFEE](FeeID),
+		constraint fk_tblEVENT_P_R_ID foreign key (P_R_ID) references [tblPERSON_ROLE](P_R_ID)
 	)
 create table [tblEVENT_TRANSMITTAL]
 	(
-		EventTransmittalID int identity(1,1) primary key,
-		EventID int not null foreign key references [tblEVENT](EventID),
-		TRANSMITTALID int not null foreign key references [tblTRANSMITTAL](TransmittalID),
+		EventTransmittalID int identity(1,1),
+		EventID int not null,
+		TransmittalID int not null,
 		amount money
+
+		constraint pk_tblEVENT_TRANSMITTAL primary key (EventTransmittalID),
+		constraint fk_tblEVENT_TRANSMITTAL_EventID foreign key (EventID) references [tblEVENT](EventID),
+		constraint fk_tblEVENT_TRANSMITTAL_TransmittalID foreign key (TransmittalID) references [tblTRANSMITTAL](TransmittalID)
 	)
 create table [tblCOMMENT]
 	(
-		CommentID int identity(1,1) primary key,
+		CommentID int identity(1,1),
 		CommentDate date not null,
-		EventID int not null foreign key references [tblEVENT](EventID),
+		EventID int not null,
 		CommentBody nvarchar(1000)
+
+		constraint pk_tblCOMMENT primary key (CommentID),
+		constraint fk_tblCOMMENT_EventID foreign key (EventID) references [tblEVENT](EventID),
 	)
