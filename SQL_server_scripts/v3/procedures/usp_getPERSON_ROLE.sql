7343d5fe580721f272c722d2961cbbd744b9bdbe
Brendan Carlquist <carlquistb@gmail.com>
2018-06-15 22:01:40 -0700

    getID for PersonRole.

---
+++ SQL_server_scripts/v3/procedures/usp_getPERSON_ROLE.sql
@@ -0,0 +1,20 @@
+create procedure usp_getPERSON_ROLE
+	(
+		@First nvarchar(50),
+		@Last nvarchar(50),
+		@DOB nvarchar(50),
+		@Role nvarchar(50),
+		@ID int output
+	)
+as
+
+set @ID = (
+	select PersonRoleID
+	 from tblPERSON_ROLE PR 
+		join tblPERSON P on P.PersonID = PR.PersonID 
+		join tblROLE R on R.RoleID = PR.RoleID 
+	where P.FName = @First 
+		and P.LName = @Last 
+		and P.DOB = @DOB 
+		and R.RoleName = @Role
+)
