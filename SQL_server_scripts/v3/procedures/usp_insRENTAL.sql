8da61f75f16deeebffdeaf7fd675f4fdde7bde9d
Brendan Carlquist <carlquistb@gmail.com>
2018-06-20 18:29:08 -0700

    getIDs and inserts.

--- SQL_server_scripts/v3/procedures/usp_insRENTAL.sql
+++ SQL_server_scripts/v3/procedures/usp_insRENTAL.sql
@@ -16,17 +16,7 @@ create procedure usp_insRENTAL
 		@EmployeeRoleName nvarchar(50)
 	)
 as
+
+execute usp_getPERSON_ROLEID
 --obtain values
 go
-
-create procedure usp_insRENTAL_internal
-	(
-		@RenterID int,
-		@EmployeeID int,
-		@EqID int,
-		@AuthorizerID int
-	)
-as
-	insert into tblRENTAL (RenterID, EmployeeID, EqID, AuthorizerID)
-		values (@RenterID, @EmployeeID, @EqID, @AuthorizerID)
-	
